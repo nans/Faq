6e4fc7d8985836742f615e13c0377ecfb4beac6f
nans <n.andry.serg@gmail.com>
2018-02-22 18:54:16 +0200

    Simplified controller.

--- Controller/Index/Index.php
+++ Controller/Index/Index.php
@@ -3,38 +3,16 @@
 namespace Nans\Faq\Controller\Index;
 
 use Magento\Framework\App\Action\Action;
-use Magento\Framework\App\Action\Context;
+use Magento\Framework\Controller\ResultFactory;
 use Magento\Framework\Controller\ResultInterface;
-use Magento\Framework\View\Result\PageFactory;
 
 class Index extends Action
 {
     /**
-     * @var PageFactory
-     */
-    protected $_resultPageFactory;
-
-    /**
-     * Constructor
-     *
-     * @param Context $context
-     * @param PageFactory $resultPageFactory
-     */
-    public function __construct(
-        Context $context,
-        PageFactory $resultPageFactory
-    ) {
-        $this->_resultPageFactory = $resultPageFactory;
-        parent::__construct($context);
-    }
-
-    /**
-     * Execute view action
-     *
      * @return ResultInterface
      */
     public function execute():ResultInterface
     {
-        return $this->_resultPageFactory->create();
+        return $this->resultFactory->create(ResultFactory::TYPE_PAGE);
     }
 }
